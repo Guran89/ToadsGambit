fa0e6a97a58105826b3c80a073137c06abaa8dc4
Kristoffer Warnberg <kristoffer.warnberg@gmail.com>
2024-08-24 16:50:06 +0200

    Minor structuring of files

--- Assets/Scripts/Player/CharacterMovement.cs
+++ Assets/Scripts/Player/CharacterMovement.cs
@@ -7,6 +7,9 @@ public class CharacterMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 6f;
+
+    public float turnSmoothTime = 0.1f;
+    float smoothTurnVelocity;
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
@@ -14,6 +17,11 @@ public class CharacterMovement : MonoBehaviour
         Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
 
         if(direction.magnitude >= 0.1f){
+
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref smoothTurnVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
             controller.Move(direction * speed * Time.deltaTime);
         }
     }
